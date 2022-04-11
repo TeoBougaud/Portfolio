bf146680137d90b64cf06b9f1e829db9073a9458
Téo BOUGAUD <tbougaud@sio.lan>
2022-04-11 10:19:15 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,270 @@
+<!DOCTYPE HTML>
+<!--
+	Hyperspace by HTML5 UP
+	html5up.net | @ajlkn
+	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
+-->
+<html>
+	<head>
+		<title>Portfolio Téo Bougaud</title>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
+		<link rel="stylesheet" href="assets/css/main.css" />
+		<noscript><link rel="stylesheet" href="assets/css/noscript.css" /></noscript>
+	</head>
+	<body class="is-preload">
+
+		<!-- Sidebar -->
+			<section id="sidebar">
+				<div class="inner">
+					<nav>
+						<ul>
+							<img src="assets/img/photo_id.jpg" alt="" class="img-fluid rounded-circle">
+							<li><a href="#intro">Bienvenue</a></li>
+							<li><a href="#one">Qui je suis</a></li>
+							<li><a href="#two">Mes compétences</a></li>
+							<li><a href="#three">Veille technologiques</a></li>
+							<li><a href="#four">Me Contacter</a></li>
+							<li><a href="projets.php">Mes Projets</a></li>
+						</ul>
+					</nav>
+				</div>
+			</section>
+
+		<!-- Wrapper -->
+			<div id="wrapper">
+
+				<!-- Intro -->
+					<section id="intro" class="wrapper style1 fullscreen fade-up">
+						<div class="inner">
+							<h1>Téo Bougaud</h1>
+							<p>Je suis étudiant en BTS SIO option SLAM</p>
+							<ul class="actions">
+								<li><a href="#one" class="button scrolly">En savoir plus</a></li>
+							</ul>
+						</div>
+					</section>
+
+				<!-- One -->
+					<section id="one" class="wrapper style2 spotlights">
+						<section>
+							
+							<div class="content">
+								<div class="inner">
+									<h2>Bonjour, je m'appelle Téo. Ravi de vous rencontrer.</h2>
+									<p>Je m’appelle Téo Bougaud, je suis né le 09/02/2002 à Lons-le-Saunier et je suis actuellement étudiant au lycée Mathias à Chalon en seconde année de BTS SIO (Services Informatiques aux Organisations) , spécialité SLAM (Solutions Logicielles et Applications Métiers). Avant mon BTS , j’ai obtenu un baccalauréat Scientifique (Bac STI2D option SIN) , puis passionné par l’informatique et plus particulièrement dans le developpement et la programation, j’ai souhaité approfondir mes connaissances dans ce domaine et poursuivre mes études afin de devenir developpeur web ou bien developpeur d'application mobile.</p>
+									</br></br>
+
+									<h2>Télécharger mon CV</h2>
+									<a href="assets/CV_Téo_Bougaud.pdf" class="btn" download target="_blank"> <span></span>Cliquez juste Ici.</a>
+										<div class="row" >
+											<div class="col-lg-4 ms-auto"><p class="lead">
+												<iframe id="inlineFrameExample"
+														title="Inline Frame Example"
+														width="900"
+														height="500"
+														src="assets/CV_Téo_Bougaud.pdf">
+												</iframe>
+											</div>
+										</div>
+
+									</br></br>
+
+									<div class="row">
+								<div class="col-lg-4 col-md-6 icon-box" data-aos="fade-up">
+									<div class="icon"><i class="bi bi-briefcase"></i></div>
+									<h4 class="title">Option SLAM (Solutions Logicielles et Applications Métiers)</h4>
+									<p class="description">Formé aux developpement web et application mobile</p>
+								</div>
+								<div class="col-lg-4 col-md-6 icon-box" data-aos="fade-up" data-aos-delay="100">
+									<div class="icon"><i class="bi bi-card-checklist"></i></div>
+									<h4 class="title">Cybersécurité des services informatiques</h4>
+									<p class="description">Formé à la cybersécurité et à son intégration dans l'entreprise en tenant compte des dimensions techniques, organisationnelles, juridiques</p>
+								</div>
+								<div class="col-lg-4 col-md-6 icon-box" data-aos="fade-up" data-aos-delay="200">
+									<div class="icon"><i class="bi bi-bar-chart"></i></div>
+									<h4 class="title">Support et mise à disposition de services informatiques</h4>
+									<p class="description">Capable de répondre aux attentes des utilisateurs.</p>
+								</div>
+								</div>
+								<h2>Certification PIX</h2>
+								<div class="row" >
+											<div class="col-lg-4 ms-auto"><p class="lead">
+												<iframe id="inlineFrameExample"
+														title="Inline Frame Example"
+														width="900"
+														height="500"
+														src="assets/attestation-pix-20220406.pdf">
+												</iframe>
+											</div>
+										</div>
+
+									</br></br>
+
+									<h3 class="resume-title">Informations complémentaires</h3>
+									<div class="resume-item"></br>
+									<ul>
+										<li>20ans</li>
+										<li>Permis B (+2 ans)</li>
+										<li>véhicule personnel</li>
+										<li>grande mobilité et disponibilité</li>
+									</ul>
+									</div>
+
+										</br></br>
+												<div class="inner">
+												<h3>Mes qualités</h3>
+													<p>Voici les différentes qualités que je possède :</p>
+
+												<p>Réactif : La réactivité permet une résolution rapide des problèmes</p>
+												<p>Persévérant : La persévérance est un fait fastidieux et long parfois, mais qui est récompensée par la satisfaction d'avoir réussi</p>
+												<p>Autonome : L'autonomie permet de mettre des solutions en place sans avoir besoin d'être assisté</p>
+														<li><a href="#two" class="button scrolly">En savoir plus</a></li>
+													</div>
+												</section>
+											</section>
+										
+
+				<!-- Two -->
+					<section id="two" class="wrapper style3 fade-up">
+						<div class="inner">
+							<h2>Mes compétences</h2>
+							<p></p>
+							<ul class="actions">
+							</ul>
+							<div class="features">
+								<section>
+									<span class="icon solid major fa-code"></span>
+									<h3>PHP</h3>
+									<p><span class="skill">Niveau <i class="val">95%</i></span></p>
+								</section>
+								<section>
+									<span class="icon solid major fa-lock"></span>
+									<h3>HTML</h3>
+									<p><span class="skill">Niveau <i class="val">95%</i></span></p>
+								</section>
+								<section>
+									<span class="icon solid major fa-cog"></span>
+									<h3>CSS</h3>
+									<p><span class="skill">Niveau <i class="val">85%</i></span></p>
+								</section>
+								<section>
+									<span class="icon solid major fa-desktop"></span>
+									<h3>C#</h3>
+									<p><span class="skill">Niveau <i class="val">65%</i></span></p>
+								</p>
+								</section>
+								<section>
+									<span class="icon solid major fa-link"></span>
+									<h3>JavaScript</h3>
+									<p><span class="skill">Niveau <i class="val">55%</i></span></p>
+								</section>
+								<section>
+									<span class="icon major fa-gem"></span>
+									<h3>SQL</h3>
+									<p><span class="skill">Niveau <i class="val">55%</i></span></p>
+								</section>
+							</div>
+
+							</br></br>
+
+							<!-- ======= Three ======= -->
+							<h2>Veille Technologique :</h2>
+							<section id="three" class="wrapper style3 fade-up">
+							<div class="container">
+
+								<div class="section-title">
+								<div class="row portfolio-container" data-aos="fade-up" data-aos-delay="100">
+
+								<!--Veille-->
+								<div class="col-lg-4 col-md-6 portfolio-item filter-app">
+									<div class="portfolio-wrap">
+									<iframe src="https://www.numerama.com/cyberguerre/" width="400px" height="250px" frameborder="0" allowfullscreen sandbox>
+									<a href="https://www.numerama.com/cyberguerre/">Lien de repli pour les navigateurs ne prenant pas en charge iframe</a>
+									</iframe>
+									<div class="portfolio-links">
+										<a href="https://www.numerama.com/cyberguerre/" title="Aller sur le site" target="_blank"><i class="bx bx-link">Aller sur le site</i></a>
+									</div>
+									</div>
+								</div>
+
+								<div class="col-lg-4 col-md-6 portfolio-item filter-app">
+									<div class="portfolio-wrap">
+									<iframe src="https://www.journaldugeek.com/hub/jeux-video/" width="400px" height="250px" frameborder="0" allowfullscreen sandbox>
+									<a href="https://www.journaldugeek.com/hub/jeux-video/">Lien de repli pour les navigateurs ne prenant pas en charge iframe</a>
+									</iframe>
+									<div class="portfolio-links">
+										<a href="https://www.journaldugeek.com/hub/jeux-video/" title="Aller sur le site" target="_blank"><i class="bx bx-link">Aller sur le site</i></a>
+									</div>
+									</div>
+								</div>
+
+								<div class="col-lg-4 col-md-6 portfolio-item filter-app">
+									<div class="portfolio-wrap">
+									<iframe src="https://nftactu.fr/" width="400px" height="250px" frameborder="0" allowfullscreen sandbox>
+									<a href="https://nftactu.fr/">Lien de repli pour les navigateurs ne prenant pas en charge iframe</a>
+									</iframe>
+									<div class="portfolio-links">
+										<a href="https://nftactu.fr/" title="Aller sur le site" target="_blank"><i class="bx bx-link">Aller sur le site</i></a>
+									</div>
+									</div>
+								</div>
+
+								<div class="col-lg-4 col-md-6 portfolio-item filter-app">
+									<div class="portfolio-wrap">
+									<iframe src="https://www.bdfugue.com/manga/nouveautes" width="400px" height="250px" frameborder="0" allowfullscreen sandbox>
+									<a href="https://www.bdfugue.com/manga/nouveautes">Lien de repli pour les navigateurs ne prenant pas en charge iframe</a>
+									</iframe>
+									<div class="portfolio-links">
+										<a href="https://www.bdfugue.com/manga/nouveautes" title="Aller sur le site" target="_blank"><i class="bx bx-link">Aller sur le site</i></a>
+									</div>
+									</div>
+								</div>
+
+								<div class="col-lg-4 col-md-6 portfolio-item filter-app">
+									<div class="portfolio-wrap">
+									<iframe src="https://mashable.com/tech" width="400px" height="250px" frameborder="0" allowfullscreen sandbox>
+									<a href="https://mashable.com/tech">Lien de repli pour les navigateurs ne prenant pas en charge iframe</a>
+									</iframe>
+									<div class="portfolio-links">
+										<a href="https://mashable.com/tech" title="Aller sur le site" target="_blank"><i class="bx bx-link">Aller sur le site </i></a>
+									</div>
+									</div>
+								</div>
+						</div>
+						</section>
+
+						</br></br>
+
+						<!--Four-->
+						<section id="four" class="wrapper style3 fade-up">
+						<div class="row">
+						<h3>Me Contacter :</h3>
+						<p class="field"><a class="button is-medium"  width="120" target="_blank" href="https://github.com/TeoBougaud"><span class="icon is-small"><span class="aws-btn aws-btn--github aws-btn--visible"><span><svg class="i-svg" height="20" version="1.1" width="15" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid meet" viewBox="0 0 50 30" space="preserve" data-icon="facebook,"><path d="M24.999,2.48c-12.75,0-23.087,10.338-23.087,23.09c0,10.199,6.613,18.854,15.791,21.907 c1.154,0.211,1.518-0.474,1.518-1.084c0-0.547,0.011-2.082,0-4.01c-6.422,1.398-7.753-3.038-7.753-3.038 c-1.048-2.671-2.562-3.377-2.562-3.377c-2.095-1.433,0.158-1.407,0.158-1.407c2.317,0.163,3.538,2.383,3.538,2.383 c2.059,3.522,5.403,2.505,6.717,1.916c0.21-1.491,0.808-2.51,1.468-3.087C15.66,35.188,10.27,33.211,10.27,24.361 c0-2.521,0.9-4.581,2.376-6.194c-0.239-0.584-1.031-2.932,0.226-6.112c0,0,1.939-0.62,6.349,2.369 c1.841-0.513,3.817-0.768,5.78-0.777c1.962,0.009,3.938,0.264,5.781,0.777c4.409-2.988,6.346-2.369,6.346-2.369 c1.258,3.18,0.466,5.528,0.229,6.112c1.478,1.613,2.373,3.673,2.373,6.194c0,8.872-5.397,10.823-10.543,11.392 c0.828,0.717,1.582,2.101,1.582,4.255c0,2.887,0,5.632,0,6.392c0,0.617,0.372,1.302,1.544,1.076 c9.167-3.059,15.776-11.708,15.776-21.905C48.089,12.818,37.75,2.48,24.999,2.48z" fill="#ffffff"></path></svg> <span></span></span></span></span></span></a></awesome-button>
+ 							<path fill="currentColor" d="M459.37 151.716c.325 4.548.325 9.097.325 13.645 0 138.72-105.583 298.558-298.558 298.558-59.452 0-114.68-17.219-161.137-47.106 8.447.974 16.568 1.299 25.34 1.299 49.055 0 94.213-16.568 130.274-44.832-46.132-.975-84.792-31.188-98.112-72.772 6.498.974 12.995 1.624 19.818 1.624 9.421 0 18.843-1.3 27.614-3.573-48.081-9.747-84.143-51.98-84.143-102.985v-1.299c13.969 7.797 30.214 12.67 47.431 13.319-28.264-18.843-46.781-51.005-46.781-87.391 0-19.492 5.197-37.36 14.294-52.954 51.655 63.675 129.3 105.258 216.365 109.807-1.624-7.797-2.599-15.918-2.599-24.04 0-57.828 46.782-104.934 104.934-104.934 30.213 0 57.502 12.67 76.67 33.137 23.715-4.548 46.456-13.32 66.599-25.34-7.798 24.366-24.366 44.833-46.132 57.827 21.117-2.273 41.584-8.122 60.426-16.243-14.292 20.791-32.161 39.308-52.628 54.253z"></path></svg><!-- <i class="fab fa-twitter fa-fw"></i> --></span></a><path fill="currentColor" d="M256 8C119.252 8 8 119.252 8 256s111.252 248 248 248 248-111.252 248-248S392.748 8 256 8zm163.97 114.366c29.503 36.046 47.369 81.957 47.835 131.955-6.984-1.477-77.018-15.682-147.502-6.818-5.752-14.041-11.181-26.393-18.617-41.614 78.321-31.977 113.818-77.482 118.284-83.523zM396.421 97.87c-3.81 5.427-35.697 48.286-111.021 76.519-34.712-63.776-73.185-116.168-79.04-124.008 67.176-16.193 137.966 1.27 190.061 47.489zm-230.48-33.25c5.585 7.659 43.438 60.116 78.537 122.509-99.087 26.313-186.36 25.934-195.834 25.809C62.38 147.205 106.678 92.573 165.941 64.62zM44.17 256.323c0-2.166.043-4.322.108-6.473 9.268.19 111.92 1.513 217.706-30.146 6.064 11.868 11.857 23.915 17.174 35.949-76.599 21.575-146.194 83.527-180.531 142.306C64.794 360.405 44.17 310.73 44.17 256.323zm81.807 167.113c22.127-45.233 82.178-103.622 167.579-132.756 29.74 77.283 42.039 142.053 45.189 160.638-68.112 29.013-150.015 21.053-212.768-27.882zm248.38 8.489c-2.171-12.886-13.446-74.897-41.152-151.033 66.38-10.626 124.7 6.768 131.947 9.055-9.442 58.941-43.273 109.844-90.795 141.978z"></path></svg><!-- <i class="fab fa-dribbble fa-fw"></i> --></span></a><a class="button is-medium" href="https://www.linkedin.com/in/Teo-Bougaud-32526014/" target="_blank"><span class="icon is-small"><svg class="svg-inline--fa fa-linkedin-in fa-fw" aria-hidden="true" focusable="false" data-prefix="fab" data-icon="linkedin-in" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" data-fa-i2svg=""><path fill="currentColor" d="M100.28 448H7.4V148.9h92.88zM53.79 108.1C24.09 108.1 0 83.5 0 53.8a53.79 53.79 0 0 1 107.58 0c0 29.7-24.1 54.3-53.79 54.3zM447.9 448h-92.68V302.4c0-34.7-.7-79.2-48.29-79.2-48.29 0-55.69 37.7-55.69 76.7V448h-92.78V148.9h89.08v40.8h1.3c12.4-23.5 42.69-48.3 87.88-48.3 94 0 111.28 61.9 111.28 142.3V448z"></path></svg><!-- <i class="fab fa-linkedin-in fa-fw"></i> --></span></a><path fill="currentColor" d="M347.1 215.4c11.7-32.6 45.4-126.9 45.4-157.1 0-26.6-15.7-48.9-43.7-48.9-44.6 0-84.6 131.7-97.1 163.1C242 144 196.6 0 156.6 0c-31.1 0-45.7 22.9-45.7 51.7 0 35.3 34.2 126.8 46.6 162-6.3-2.3-13.1-4.3-20-4.3-23.4 0-48.3 29.1-48.3 52.6 0 8.9 4.9 21.4 8 29.7-36.9 10-51.1 34.6-51.1 71.7C46 435.6 114.4 512 210.6 512c118 0 191.4-88.6 191.4-202.9 0-43.1-6.9-82-54.9-93.7zM311.7 108c4-12.3 21.1-64.3 37.1-64.3 8.6 0 10.9 8.9 10.9 16 0 19.1-38.6 124.6-47.1 148l-34-6 33.1-93.7zM142.3 48.3c0-11.9 14.5-45.7 46.3 47.1l34.6 100.3c-15.6-1.3-27.7-3-35.4 1.4-10.9-28.8-45.5-119.7-45.5-148.8zM140 244c29.3 0 67.1 94.6 67.1 107.4 0 5.1-4.9 11.4-10.6 11.4-20.9 0-76.9-76.9-76.9-97.7.1-7.7 12.7-21.1 20.4-21.1zm184.3 186.3c-29.1 32-66.3 48.6-109.7 48.6-59.4 0-106.3-32.6-128.9-88.3-17.1-43.4 3.8-68.3 20.6-68.3 11.4 0 54.3 60.3 54.3 73.1 0 4.9-7.7 8.3-11.7 8.3-16.1 0-22.4-15.5-51.1-51.4-29.7 29.7 20.5 86.9 58.3 86.9 26.1 0 43.1-24.2 38-42 3.7 0 8.3.3 11.7-.6 1.1 27.1 9.1 59.4 41.7 61.7 0-.9 2-7.1 2-7.4 0-17.4-10.6-32.6-10.6-50.3 0-28.3 21.7-55.7 43.7-71.7 8-6 17.7-9.7 27.1-13.1 9.7-3.7 20-8 27.4-15.4-1.1-11.2-5.7-21.1-16.9-21.1-27.7 0-120.6 4-120.6-39.7 0-6.7.1-13.1 17.4-13.1 32.3 0 114.3 8 138.3 29.1 18.1 16.1 24.3 113.2-31 174.7zm-98.6-126c9.7 3.1 19.7 4 29.7 6-7.4 5.4-14 12-20.3 19.1-2.8-8.5-6.2-16.8-9.4-25.1z"></path></svg><!-- <i class="fab fa-angellist fa-fw"></i> --></span></a><path fill="currentColor" d="M326.3 218.8c0 20.5-16.7 37.2-37.2 37.2h-70.3v-74.4h70.3c20.5 0 37.2 16.7 37.2 37.2zM504 256c0 137-111 248-248 248S8 393 8 256 119 8 256 8s248 111 248 248zm-128.1-37.2c0-47.9-38.9-86.8-86.8-86.8H169.2v248h49.6v-74.4h70.3c47.9 0 86.8-38.9 86.8-86.8z"></path></svg><!-- <i class="fab fa-product-hunt fa-fw"></i> --></span></a><a class="button is-medium" href="mailto:teo39463@gmail.com" target="_blank"><span class="icon is-small"><svg class="svg-inline--fa fa-envelope fa-fw" aria-hidden="true" focusable="false" data-prefix="far" data-icon="envelope" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" data-fa-i2svg=""><path fill="currentColor" d="M0 128C0 92.65 28.65 64 64 64H448C483.3 64 512 92.65 512 128V384C512 419.3 483.3 448 448 448H64C28.65 448 0 419.3 0 384V128zM48 128V150.1L220.5 291.7C241.1 308.7 270.9 308.7 291.5 291.7L464 150.1V127.1C464 119.2 456.8 111.1 448 111.1H64C55.16 111.1 48 119.2 48 127.1L48 128zM48 212.2V384C48 392.8 55.16 400 64 400H448C456.8 400 464 392.8 464 384V212.2L322 328.8C283.6 360.3 228.4 360.3 189.1 328.8L48 212.2z"></path></svg><!-- <i class="far fa-envelope fa-fw"></i> --></span></a></p>
+ 							<!--<li><i class="bi bi-chevron-right"></i> <strong>Site Web :</strong> <a href="https://bougaud.alwaysdata.net/index.php"><span>https://bougaud.alwaysdata.net/index.php</span></a></li>-->
+						</div>
+						</section>
+			</div>
+
+		<!-- Footer -->
+			<footer id="footer" class="wrapper style1-alt">
+				<div class="inner">
+					<ul class="menu">
+						<li>&copy; Untitled. All rights reserved.</li><li>Design by :Téo Bougaud </li>
+						
+					</section>
+					</ul>
+				</div>
+			</footer>
+		<!-- Scripts -->
+			<script src="assets/js/jquery.min.js"></script>
+			<script src="assets/js/jquery.scrollex.min.js"></script>
+			<script src="assets/js/jquery.scrolly.min.js"></script>
+			<script src="assets/js/browser.min.js"></script>
+			<script src="assets/js/breakpoints.min.js"></script>
+			<script src="assets/js/util.js"></script>
+			<script src="assets/js/main.js"></script>
+
+	</body>
+</html>
---
+++ projets.php
@@ -0,0 +1,114 @@
+<!DOCTYPE HTML>
+<!--
+	Hyperspace by HTML5 UP
+	html5up.net | @ajlkn
+	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
+-->
+<html>
+	<head>
+		<title>Projets</title>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
+		<link rel="stylesheet" href="assets/css/main.css" />
+		<noscript><link rel="stylesheet" href="assets/css/noscript.css" /></noscript>
+	</head>
+	<body class="is-preload">
+
+		<!-- Header -->
+			<header id="header">
+				<a href="index.php" class="title">Téo Bougaud</a>
+				<nav>
+					<ul>
+						<li><a href="index.php #intro">Accueil</a></li>
+						<li><a href="index.php #one">Qui je suis</a></li>
+						<li><a href="index.php #two">Mes compétences</a></li>
+						<li><a href="index.php #three">Veille technologiques</a></li>
+						<li><a href="index.php #four">Me Contacter</a></li>
+                        <li><a href="projets.php"class="active">Mes Projets</a></li>
+					</ul>
+				</nav>
+			</header>
+
+		<!-- Wrapper -->
+			<div id="wrapper">
+
+				<!-- Main -->
+					<section id="main" class="wrapper">
+						<div class="inner">
+							<h1 class="major">Mes Projets</h1>
+							<h2>Projets en entreprise(Stage) :</h2>
+                            <section>
+									<span class="icon solid major fa-code"></span>
+									<h3>1er Projet : Entreprise K-Services</h3>
+									<p>Creation d'un page pour les bons de retours en JS a partir d'un site de vente en ligne deja existant.</br>
+									J'ai appris a utiliser le JavaScirpt pour la première fois.</br>
+									Le but premier étais de permettre aux clients de générer un bon de retours en pdf depuis un formulaire afin qu'ils puissent l'imprimer et le joindre a colis retourné.</br> 
+									<a href="assets/img/Projet1/Bon de retour - 2021-06-14T090349.725.pdf" class="btn" download target="_blank"> <span></span>Cliquez juste Ici pour télécharger un exemple de bon de retour.</a></p>
+									<img src="assets/img/Projet1/CaptureFormulaireRetour.PNG" alt="" class="img-fluid rounded-circle">
+									
+							</section>
+                            <section>
+									<span class="icon solid major fa-code"></span>
+									<h3>2eme Projet : Entreprise K-Services</h3>
+									<p> Creation de mails personnaliser a l'aide de l'Api Email Emarsys.</br>
+									J'ai participer a la modification de l'aspect de chaque type de mail et aussi à l'appelle de l'API depuis le code.</br>
+									Le but était donc de crée des mail automatiquement envoyer au client avec les bonnes information(le prénom du client son nom....) mais aussi que les mails soit beaux et a l'image de l'entreprise.
+									</br> Exemple d'Email personnaliser :</p>
+									<img src="assets/img/CaptureEmailExemple.PNG" alt="" class="img-fluid rounded-circle">
+							</section>
+                            <section>
+									<span class="icon solid major fa-code"></span>
+									<h3>3eme Projet : Entreprise K-Services</h3>
+									<p>Creation d'un application en C# pour gerer les réunion en interne.
+									J'ai Coder et crée en C# toute l'interface utilisateur de l'application qui affiche devant chaque salle de réunion
+									si cette salle est occuper ou non, de quelle heure a quelle heure est elle occupée et un bref intitulé de la réunion.
+									</br>Voici un exemple ci dessous :</p>
+									<img src="assets/img/Projet2/urn_aaid_sc_US_185a0ce7-218a-4c5f-a9d7-ae17da563d18.png" alt="" class="img-fluid rounded-circle">
+							</section>
+							<h2>Projets en cours de formation :</h2>
+							<section>
+									<span class="icon solid major fa-code"></span>
+									<h3>1er Projet :Bac STI2D</h3>
+									<p>Creation d'une borne d'arcade:
+									J'ai participer a la creation d'une borne d'arcade de A à Z en groupe de 4 (Qui sera bientôt mis en place au foyer du lycée, dans le but de divertir les élèves pendant leur temps libre).</br>
+									Sur ce projet j'ai participer a la construction de la borne en bois, sur la partie électronique et sur la partie programation(sur raspberry pi). </p>
+							</section>
+							<section>
+									<span class="icon solid major fa-code"></span>
+									<h3>2eme Projet :BTS SIO 1</h3>
+									<p>Projet Gestion Formation:
+									Dans ce projet j'ai crée un site web (en PHP, HTML, CSS) proposant des formation Word, Linux, Exel... </br>
+									Toutes les information des clients et des formation proposer devais être gerer depuis la base de données</p>
+							</section>
+							<section>
+									<span class="icon solid major fa-code"></span>
+									<h3>3eme Projet :BTS SIO 2</h3>
+									<p>Projet Infotools :
+									Dans ce projets j'ai réaliser,</br> 
+									- Un site internet de présentation de l’entreprise et de ses produits phares.(en PHP, HTML, CSS et SQL).</br>
+									- Application CRM Web (préconisation : PHP Laravel):</br>
+									Mise en place du portail CRM (Avec écran d’accueil personnalisé sur les rendez-vous à venir)</br>
+									Récupération des données de l’Active Directory via PHP pour s’authentifier à l’application de CRM web (accès seulement aux commerciaux)</br>
+									Une page présentant les produits et Une page permettant de récapituler les achats d’un client</p>
+							</section>
+
+			<!-- Footer -->
+			<footer id="footer2" class="wrapper style1-alt">
+				<div class="inner">
+					<ul class="menu">
+						<li>&copy; Untitled. All rights reserved.</li><li>Design by :Téo Bougaud </li>
+					</ul>
+				</div>
+			</footer>
+
+			<!-- Scripts -->
+			<script src="assets/js/jquery.min.js"></script>
+			<script src="assets/js/jquery.scrollex.min.js"></script>
+			<script src="assets/js/jquery.scrolly.min.js"></script>
+			<script src="assets/js/browser.min.js"></script>
+			<script src="assets/js/breakpoints.min.js"></script>
+			<script src="assets/js/util.js"></script>
+			<script src="assets/js/main.js"></script>
+
+	</body>
+</html>
